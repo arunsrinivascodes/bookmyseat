7fff48b3336f069f949c3dd1dedf7dd2e70df297
arun@kamaltechnologies.in <arun@kamaltechnologies.in>
2020-08-19 14:19:55 +0530

    Footer styling changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <footer className="page-footer">Developed by Arun Srinivas&nbsp;&nbsp;<a href="mailto:arunsrinivas10@yahoo.com" target="_blank" rel="noopener noreferrer">Email me</a>&nbsp;&nbsp;<a href="tel:+91-8838021187" rel="noopener noreferrer" target="_blank">Call me</a></footer>
+    <footer className="page-footer">
+      <h4>Developed by Arun Srinivas</h4>
+      <section className="links">
+        <a href="mailto:arunsrinivas10@yahoo.com" target="_blank" rel="noopener noreferrer">Email me</a>
+        <a href="tel:+91-8838021187" rel="noopener noreferrer" target="_blank">Call me</a>
+      </section>
+    </footer>
   );
 }
 
